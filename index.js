f85965a55a5291ee3766c60294ccc9b761364ea9
Jose <jozylopez27@gmail.com>
2020-01-09 16:09:44 -0600

    added array of questions

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+const store = {
+
+    questions: [
+      {
+        question: 'Which of these soccer teams plays in the English Premiere League?',
+        answers: [
+          'westham united',
+          'leeds united',
+          'nottingham forrest',
+          'london united'
+        ],
+        correctAnswer: 'westham united'
+      },
+      {
+        question: 'Who is the all-time leading scorer in the English Premiere League',
+        answers: [
+          'wayne rooney',
+          'alan shearer',
+          'robbie fowler',
+          'lionel messi'
+        ],
+        correctAnswer: 'alan shearer'
+      },
+      {
+        question: 'who won the 2015-2016 English Premiere League Title',
+        answers: [
+          'Manchester united',
+          'arsenal',
+          'leicester city',
+          'liverpool'
+        ],
+        correctAnswer: 'leicester city'
+      },
+      {
+        question: 'who won the 2015-2016 English Premiere League Title',
+        answers: [
+          'Manchester united',
+          'arsenal',
+          'leicester city',
+          'liverpool'
+        ],
+        correctAnswer: 'leicester city'
+      }
+    ],
+    questionNumber: 0,
+    score: 0
+  };
